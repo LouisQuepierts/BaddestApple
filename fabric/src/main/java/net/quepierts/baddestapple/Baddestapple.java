6b7e126b3c732e79a0baffe1501d58a7fabd177e
Louis_Quepierts <LouisQuepierts@outlook.com>
2025-05-17 16:55:22 +0800

    some changes

--- fabric/src/main/java/net/quepierts/baddestapple/Baddestapple.java
+++ fabric/src/main/java/net/quepierts/baddestapple/Baddestapple.java
@@ -1,11 +1,35 @@
 package net.quepierts.baddestapple;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback;
+import net.fabricmc.fabric.api.event.client.player.ClientPreAttackCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.quepierts.baddestapple.client.ShaderHolder;
+import net.quepierts.baddestapple.client.Shaders;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 public class Baddestapple implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        CoreShaderRegistrationCallback.EVENT.register(this::onRegisterShaders);
 
+        // TODO
+    }
+
+    private void onRegisterShaders(final CoreShaderRegistrationCallback.RegistrationContext context) throws IOException {
+        for (Iterator<ShaderHolder> it = Shaders.shaders(); it.hasNext();) {
+            ShaderHolder shader = it.next();
+            context.register(shader.getLocation(), shader.getFormat(), shader::setInstance);
+        }
     }
 }
--- neoforge/src/main/java/net/quepierts/baddestapple/Baddestapple.java
+++ neoforge/src/main/java/net/quepierts/baddestapple/Baddestapple.java
@@ -1,19 +1,14 @@
 package net.quepierts.baddestapple;
 
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
-@Mod(Constants.MOD_ID)
+@Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class BaddestApple {
 
     public BaddestApple(IEventBus eventBus) {
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
         Constants.LOG.info("Hello NeoForge world!");
-        CommonClass.init();
     }
 }
